897b69a6c62708f36bfba25a183507b0a4fa0613
RomaKataria <k.roma873@gmail.com>
2021-03-16 14:02:29 +0000

    lab3

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,23 +1,47 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 import java.util.Map;
 
+@RestController
 public class EnvController {
 
-    private final string port;
-    private final string memoryLimit;
-    private  final string instanceindex;
-    private  final string instanceaddress;
+    private final String port;
+    private final String memoryLimit;
+    private final String cfInstanceIndex;
+    private final String cfInstanceAddress;
 
 
 
-    public EnvController(String port, String memoryLimit, String instanceindex, String instanceaddress) {
+
+    public EnvController(
+            @Value("${port:NOT SET}") String port,
+            @Value("${memory.limit:NOT SET}") String memoryLimit,
+            @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+            @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddress
+    ) {
         this.port = port;
-        this.memoryLimit =memoryLimit;
-        this.in
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddress = cfInstanceAddress;
     }
 
+
+
+
+    @GetMapping("/env")
     public Map<String, String> getEnv() {
-        return null;
+        Map<String, String> env = new HashMap<>();
+
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+
+        return env;
     }
 }
