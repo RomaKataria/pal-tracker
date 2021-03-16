96229cbf4365c3a45cba48aaf419d462875265d4
RomaKataria <k.roma873@gmail.com>
2021-03-16 12:26:56 +0000

    lab3

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,23 @@
+package io.pivotal.pal.tracker;
+
+import java.util.Map;
+
+public class EnvController {
+
+    private final string port;
+    private final string memoryLimit;
+    private  final string instanceindex;
+    private  final string instanceaddress;
+
+
+
+    public EnvController(String port, String memoryLimit, String instanceindex, String instanceaddress) {
+        this.port = port;
+        this.memoryLimit =memoryLimit;
+        this.in
+    }
+
+    public Map<String, String> getEnv() {
+        return null;
+    }
+}
